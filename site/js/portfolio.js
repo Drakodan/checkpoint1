423b8dd9b813e445c092738d0f95d11560e2c5ae
Drakodan <traxxdata@laposte.net>
2022-06-01 15:49:31 +0200

    Changement des h2 de .skill, footer

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,14 +4,26 @@ changeAvatar.addEventListener('click', function () {
     changeAvatar.src = "image/avatar.svg";
 });
 
+
 const changeText = document.querySelector(".button");
 const changeName = document.querySelector("#name");
-
+const changeBgColor = document.querySelectorAll(".pink-bg");
+const changeColor = document.querySelectorAll("h2.pink-text");
 
 changeText.addEventListener("click", function() {
     const myName = prompt("Entrez votre prénom");
+    const myColor = prompt("Chosissez une couleur");
     changeName.style.color = "white";
-    changeName.innerHTML =(myName)
-  }); 
+    changeName.innerHTML =(myName);
+    for(let j = 0; j < changeBgColor.length; j ++) {
+        changeBgColor[j].style.backgroundColor = myColor;
+    }
+    for(let i = 0; i < changeColor.length; i ++) {
+        changeColor[i].style.color = myColor;
+    }
+}); 
 
-  
+
+
+    
+    
